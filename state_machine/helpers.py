b1f0113a2891325d546f46ce53536d8449a677e9
Matthew Hanley <mattdhanley@gmail.com>
2019-04-17 13:59:22 -0600

    commit

--- state_machine/helpers.py
+++ state_machine/helpers.py
@@ -274,7 +274,7 @@ def set_motor_speed(serial_port, id, speed):
     # motor_speed_str = speed.to_bytes(1, byteorder='little', signed=True)
     # motor_speed_str = bytes([int(speed)])
     # motor_speed_str = str(speed)+'\r\n'
-    motor_speed_str = struct.pack('>B', speed)
+    motor_speed_str = struct.pack('>B', int(speed))
     serial_port.write(motor_speed_str)
 
     # read_ser=serial_port.readline()
