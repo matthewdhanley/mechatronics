cf5d3b100da37ca51ad08d417c327d8e7ec1e281
Matthew Hanley <mattdhanley@gmail.com>
2019-04-18 18:50:46 -0600

    asdf

--- state_machine/helpers.py
+++ state_machine/helpers.py
@@ -283,7 +283,7 @@ def set_motor_speed(serial_port, id, speed):
     motor_speed_str = motor_id_str + '\r\n' + motor_speed_str
     print(motor_speed_str)
     serial_port.write('{}\r\n{}'.format(int(id), int(speed)).encode())
-
+    time.sleep(0.3)
     # read_ser=serial_port.readline()
     # print("reading speed:" + read_ser)
 
