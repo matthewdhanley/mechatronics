e619909c8ff8f71a14f32dfc3b7a4c08f055a530
Matthew Hanley <mattdhanley@gmail.com>
2019-04-18 18:42:08 -0600

    asdf

--- state_machine/helpers.py
+++ state_machine/helpers.py
@@ -17,7 +17,7 @@ import struct
 with np.load(os.path.abspath(os.path.join(os.path.dirname(__file__), 'camera_cal_output.npz'))) as X:
     MTX, DIST, _, _ = [X[i] for i in ('mtx', 'dist', 'rvecs', 'tvecs')]
 
-DRIVE_SPEED = 20
+DRIVE_SPEED = 50
 
 def get_camera():
     """
